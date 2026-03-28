d0290c65badc080407caebe35d277be8ee92ca5d
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-03-28 17:46:38 -0300

    feat: this commit  created database settings

---
+++ src/WalletManager.CrossCutting/Models/PostgresSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace WalletManager.CrossCutting.Models
+{
+    public class PostgresSettings
+    {
+        public string ConnectionString { get; set; } = default!;
+    }
+}
---
+++ src/WalletManager.Infrastructure/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WalletManager.Domain.Enities;
+
+namespace WalletManager.Infrastructure.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        }
+
+        public DbSet<WalletEntity> Wallets { get; set; }
+        public DbSet<TransactionEntity> Transactions { get; set; }
+        public DbSet<CustomerEntity> Customers { get; set; }
+    }
+}
