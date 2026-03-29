97f71b1a204c8743ede11a311a7f9c78a05306f3
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-03-29 03:17:47 -0300

    feat: This commit creates the database extension method.

---
+++ src/WalletManager.CrossCutting/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WalletManager.CrossCutting.Models;
+using WalletManager.Infrastructure.Context;
+
+namespace WalletManager.CrossCutting.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, PostgresSettings postgresSettings)
+        {
+            services.AddDbContext<DataContext>(config =>
+            {
+                config.UseNpgsql(postgresSettings.ConnectionString);
+                config.EnableSensitiveDataLogging();
+            });
+
+            return services;
+        }
+    }
+}
