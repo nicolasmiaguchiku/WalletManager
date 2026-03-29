299a435ac8b67debb9750e3a20a99c368b5a164b
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-03-29 07:16:55 -0300

    feat: This commit creates the token provider service.

---
+++ src/WalletManager.Infrastructure/Models/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace WalletManager.Infrastructure.Models
+{
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; } = default!;
+        public double TokenValidityInMinutes { get; set; }
+        public string Issuer { get; set; } = default!;
+        public string Audience { get; set; } = default!;
+    }
+}
---
+++ src/WalletManager.Infrastructure/Services/TokenProvider.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WalletManager.Domain.Interfaces.Services;
+using WalletManager.Infrastructure.Models;
+
+namespace WalletManager.Infrastructure.Services
+{
+    public class TokenProvider(IOptions<JwtSettings> settings) : ITokenProvider
+    {
+        private readonly JwtSecurityTokenHandler _tokenHandler = new();
+
+        public JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var key = settings.Value.SecretKey;
+
+            var privateKey = Encoding.UTF8.GetBytes(key);
+
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(privateKey),
+                                            SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(settings.Value.TokenValidityInMinutes),
+                Audience = settings.Value.Audience,
+                Issuer = settings.Value.Issuer,
+                SigningCredentials = signingCredentials
+            };
+
+            var token = _tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+
+            return token;
+        }
+    }
+}
