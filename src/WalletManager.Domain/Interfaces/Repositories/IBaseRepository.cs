5b9b3d151f17d54f8a6b9ddced0bd539296eeb85
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-03-30 14:16:37 -0300

    feat: this commit create interface base repository

---
+++ src/WalletManager.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace WalletManager.Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task InsertAsync(TEntity entity, CancellationToken cancellationToken);
+
+        Task UpdateAsync(Expression<Func<TEntity, bool>> filterExpression,
+            TEntity entity, CancellationToken cancellationToken);
+
+        Task<long> DeleteAsync(Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken);
+    }
+}
