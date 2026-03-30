0fc7e0c961353f496311b1e4a72d98c29fbe5954
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-03-30 15:14:06 -0300

    feat: This commit introduces the implementation of the base repository.

---
+++ src/WalletManager.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using WalletManager.Domain.Interfaces.Repositories;
+using WalletManager.Infrastructure.Context;
+
+namespace WalletManager.Infrastructure.Repositories
+{
+    public class BaseRepository<TEntity>(DataContext dataContext) : IBaseRepository<TEntity> where TEntity : class
+    {
+        public Task<long> DeleteAsync(Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync(TEntity entity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Expression<Func<TEntity, bool>> filterExpression, TEntity entity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
