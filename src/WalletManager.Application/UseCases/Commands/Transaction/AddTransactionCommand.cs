077fe3a8ad350aa72f9bfa5b111b198a761a8e67
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-04-02 22:46:16 -0300

    feat: This commit introduces the creation of transaction request classes.

---
+++ src/WalletManager.Application/UseCases/Commands/Transaction/AddTransactionCommand.cs
@@ -0,0 +1,7 @@
+﻿using LiteBus.Commands.Abstractions;
+using WalletManager.Application.Requests;
+using WalletManager.Domain.Base;
+
+namespace WalletManager.Application.UseCases.Commands.Transaction;
+
+public record AddTransactionCommand(AddTransactionRequest Request) : ICommand<Result<Guid>>;
