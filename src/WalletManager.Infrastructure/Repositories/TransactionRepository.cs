9c05de38a78f1b87c6601c80d245950ef74e7d5a
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-04-02 22:55:15 -0300

    feat: this commit introduces transaction repository

---
+++ src/WalletManager.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,8 @@
+﻿using WalletManager.Domain.Enities;
+using WalletManager.Domain.Interfaces.Repositories;
+using WalletManager.Infrastructure.Context;
+
+namespace WalletManager.Infrastructure.Repositories;
+
+public class TransactionRepository(DataContext dataContext) :
+    BaseRepository<TransactionEntity>(dataContext), ITransactionRepository;
