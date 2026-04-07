6a35a64b70f7fc863503123590f1f3beb7c062e5
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-04-07 07:22:55 -0300

    feat: create mapper user

--- src/WalletManager.Application/UseCases/Commands/Customer/LoginCommandHandler.cs
+++ src/WalletManager.Application/UseCases/Commands/Customer/LoginCommandHandler.cs
@@ -1,14 +1,26 @@
 ﻿using LiteBus.Commands.Abstractions;
+using WalletManager.Application.Mappers;
 using WalletManager.Domain.Base;
+using WalletManager.Domain.Interfaces.Repositories;
+using WalletManager.Domain.Interfaces.Services;
 using WalletManager.Domain.ValueObjects;
 
 namespace WalletManager.Application.UseCases.Commands.Customer
 {
-    public sealed class LoginCommandHandler() : ICommandHandler<LoginCommand, Result<Token>>
+    public sealed class LoginCommandHandler(
+        ICustomerRepository customerRepository,
+        IAuthenticationService authenticationService) : ICommandHandler<LoginCommand, Result<Token>>
     {
-        public Task<Result<Token>> HandleAsync(LoginCommand message, CancellationToken cancellationToken = default)
+        public async Task<Result<Token>> HandleAsync(LoginCommand message, CancellationToken cancellationToken = default)
         {
-            
+            var customer = await customerRepository.GetAsync(x => x.Email == message.Request.Email, cancellationToken);
+
+            if(customer is null)
+            {
+                return Result<Token>.Failure(customer);
+            }
+
+            var authenticationResult = authenticationService.Authenticate(customer.ToUser());
         }
     }
 }
