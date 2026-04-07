0ce870cc6102d90ea87bc6818a7340bfd18dbe85
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-04-07 07:12:47 -0300

    feat: This commit introduces the login logic.

---
+++ src/WalletManager.Application/UseCases/Commands/Customer/LoginCommand.cs
@@ -0,0 +1,8 @@
+﻿using LiteBus.Commands.Abstractions;
+using WalletManager.Application.Requests;
+using WalletManager.Domain.Base;
+using WalletManager.Domain.ValueObjects;
+
+namespace WalletManager.Application.UseCases.Commands.Customer;
+
+public record LoginCommand(LoginRequest Request) : ICommand<Result<Token>>;
---
+++ src/WalletManager.Application/UseCases/Commands/Customer/LoginCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using LiteBus.Commands.Abstractions;
+using WalletManager.Domain.Base;
+using WalletManager.Domain.ValueObjects;
+
+namespace WalletManager.Application.UseCases.Commands.Customer
+{
+    public sealed class LoginCommandHandler() : ICommandHandler<LoginCommand, Result<Token>>
+    {
+        public Task<Result<Token>> HandleAsync(LoginCommand message, CancellationToken cancellationToken = default)
+        {
+            
+        }
+    }
+}
